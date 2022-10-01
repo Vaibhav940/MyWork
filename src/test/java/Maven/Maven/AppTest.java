ca667da1f108d12366d8e61cebf7effbebe4b7e9
Vaibhav940 <vaibhavpanchal869@gmail.com>
2022-10-01 23:06:34 +0530

    mywork

---
+++ src/test/java/Maven/Maven/AppTest.java
@@ -0,0 +1,16 @@
+package Maven.Maven;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit vTest for simple App.
+ */
+public class AppTest 
+{
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Vaibhav");
+    }
+}
---
+++ src/test/java/Maven/Maven/vTest.java
@@ -0,0 +1,14 @@
+package Maven.Maven;
+
+import org.testng.annotations.Test;
+
+public class vTest {
+
+	    
+	    @Test
+	    public void test()
+	    {
+	        System.out.println("Vaibhav2");
+	    }
+	}
+
