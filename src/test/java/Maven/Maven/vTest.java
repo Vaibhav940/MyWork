1a725d0aea21cca852ff3ade346b2827909c6a07
Vaibhav9404 <vaibhavpanchal869@gmail.com>
2022-10-02 19:15:18 +0530

    fffff

--- src/test/java/Maven/Maven/vTest.java
+++ src/test/java/Maven/Maven/vTest.java
@@ -8,11 +8,9 @@ public class vTest {
 	public void test() {
 		System.out.println("Vaibhav2");
 		System.out.println("shlok is good person ");
-		System.out.println("nanded ");
-		System.out.println("vaibhav ");
-		System.out.println("vaibhav ");
-		System.out.println("vaibhav ");
-		System.out.println("vaibhav ");
+	     System.out.println("dddddddd");
+	     System.out.println("frvfbvufeb[ug hf ");
+	     
 
 	}
 }
